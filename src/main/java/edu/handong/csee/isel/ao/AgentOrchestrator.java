72baab4449b2510cc07cad4dad265160e8e32747
Hahyun Baek <hahyun8587@gmail.com>
2025-12-09 23:51:30 +0900

    feat: support router

--- src/main/java/edu/handong/csee/isel/ao/AgentOrchestrator.java
+++ src/main/java/edu/handong/csee/isel/ao/AgentOrchestrator.java
@@ -3,6 +3,8 @@ package edu.handong.csee.isel.ao;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 
 import com.google.protobuf.ByteString;
@@ -12,12 +14,15 @@ import io.grpc.internal.JsonParser;
 import edu.handong.csee.isel.ao.network.ROSSimulator;
 import edu.handong.csee.isel.ao.network.client.DataStoringClient;
 import edu.handong.csee.isel.ao.network.server.ActionReceivingServer;
+import edu.handong.csee.isel.ao.policy.Router;
 import edu.handong.csee.isel.proto.*;
 
 public class AgentOrchestrator implements AutoCloseable {
     private ROSSimulator simulator;
     private ActionReceivingServer server;
     private DataStoringClient client;
+    private Router router;
+    private Collection<Thread> threads;
 
     public AgentOrchestrator(Path config) throws IOException {
         Object jsonObject = JsonParser.parse(Files.readString(config));
@@ -31,14 +36,107 @@ public class AgentOrchestrator implements AutoCloseable {
         server = new ActionReceivingServer(
                 this, (Integer) jsonMap.get("port"));
         client = new DataStoringClient();
+        router = new Router();
+        threads = new ArrayList<>();
     }
 
-    public void listenRobot() {
+    public void run() throws IOException {
         simulator.start();
+        server.start();
     }
 
-    public void listenAgents() throws IOException {
-        server.start();
+    public void update(AgentInfo.AgentType type, String host, int port) {
+        Thread thread;
+        
+        client.addStub(type, host, port);
+        thread = new Thread(
+                () -> { 
+                    try {
+                        while (true) {
+                            client.sendData(type, 30);
+                            Thread.sleep(1);
+                        } 
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+        threads.add(thread);
+        thread.start();
+    }
+
+    public void update(
+            byte[] image, byte[] depth, float accel, float angular, 
+            float mag_str_x, float mag_str_y, String target, String text,
+            byte[] header, byte[] format, byte[] data) {
+        for (AgentInfo.AgentType type
+                : router.route(
+                        image, depth, accel, angular, 
+                        mag_str_x, mag_str_y, target, 
+                        text, header, format, data)) {
+                            
+            switch (type) {
+                case AT_UNSPECIFIED:
+
+                case AT_ISA:
+                    client.addData(
+                            type, 
+                            convertToClientFormat(
+                                    image, depth, accel, angular, 
+                                    mag_str_x, mag_str_y, target, text));
+                    
+                    break;
+                
+                case AT_IUA:
+                    client.addData(
+                            type, 
+                            convertToClientFormat(
+                                    image, depth, header, format, data));
+
+                    break;
+
+                case AT_IOA:
+                
+                case UNRECOGNIZED:
+            }
+        }
+    }
+
+    private Data convertToClientFormat(
+            byte[] image, byte[] depth, float accel, float angular, 
+            float mag_str_x, float mag_str_y, String target, String text) {
+        Data.Builder dataBuilder = Data.newBuilder();
+        DataISA.Builder dataISABuilder = dataBuilder.getDataISABuilder();
+
+        dataISABuilder.getRgbdBuilder()
+                      .setImage(ByteString.copyFrom(image))
+                      .setDepth(ByteString.copyFrom(depth));
+        dataISABuilder.getImuBuilder()
+                      .setAccel(accel)
+                      .setAngular(angular)
+                      .setMagStrX(mag_str_x)
+                      .setMagStrY(mag_str_y);
+        dataISABuilder.getCmdBuilder()
+                      .setTarget(target)
+                      .setTarget(text);
+        
+        return dataBuilder.build();
+    }
+
+    private Data convertToClientFormat(
+            byte[] image, byte[] depth, 
+            byte[] header, byte[] format, byte[] data) {
+        Data.Builder dataBuilder = Data.newBuilder();
+        DataIUA.Builder dataIUABuilder = DataIUA.newBuilder();
+
+        dataIUABuilder.getRgbdBuilder()
+                      .setImage(ByteString.copyFrom(image))
+                      .setDepth(ByteString.copyFrom(depth));
+        dataIUABuilder.getVoiceBuilder()
+                      .setHeader(ByteString.copyFrom(header))
+                      .setFormat(ByteString.copyFrom(format))
+                      .setFormat(ByteString.copyFrom(data));
+        
+        return dataBuilder.build();
     }
 
     public void update(
@@ -60,69 +158,32 @@ public class AgentOrchestrator implements AutoCloseable {
                 angularX, angularY, angularZ);
     }
 
-    public void update(AgentInfo.AgentType type, String host, int port) {
-        client.addStub(type, host, port);
-    }
-
-    public void update(
-            byte[] image, byte[] depth, float accel, float angular, 
-            float mag_str_x, float mag_str_y, String target, String text,
-            byte[] header, byte[] format, byte[] data) {
-        client.addRGBD(convertToClientFormat(image, depth));
-        client.addIMU(
-                convertToClientFormat(accel, angular, mag_str_x, mag_str_y));
-        client.addUserCmd(convertToClientFormat(target, text));
-        client.addVoice(convertToClientFormat(header, format, data));
-    }
-
-    private RGBD convertToClientFormat(byte[] image, byte[] depth) {
-        return RGBD.newBuilder()
-                   .setImage(ByteString.copyFrom(image))
-                   .setDepth(ByteString.copyFrom(depth))
-                   .build();
-    }
-
-    private IMU convertToClientFormat(
-            float accel, float angular, float mag_str_x, float mag_str_y) {
-        return IMU.newBuilder()
-                  .setAccel(accel)
-                  .setAngular(angular)
-                  .setMagStrX(mag_str_x)
-                  .setMagStrY(mag_str_y)
-                  .build();
-    }
-
-    private UserCmd convertToClientFormat(String target, String text) {
-        return UserCmd.newBuilder()
-                      .setTarget(target)
-                      .setTarget(text)
-                      .build();
-    }
-
-    private Voice convertToClientFormat(
-            byte[] header, byte[] format, byte[] data) {
-        return Voice.newBuilder()
-                    .setHeader(ByteString.copyFrom(header))
-                    .setFormat(ByteString.copyFrom(format))
-                    .setData(ByteString.copyFrom(data))
-                    .build();
-    }
-
     public void update() {
         
     }
 
-    public void close() {
-        server.shutdownNow();
-        client.shutdownNow();
+    public void close() throws InterruptedException {
+        server.shutdown();
+        
+        for (Thread thread : threads) {
+            thread.interrupt();
+            
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        client.shutdown();
     }
 
     public static void main(String[] args) {
-        try (AgentOrchestrator ao = new AgentOrchestrator(Path.of(args[0]))) {
-            ao.listenRobot();
-            ao.listenAgents();
+        try (AgentOrchestrator ao = new AgentOrchestrator(
+                Path.of(ClassLoader.getSystemResource("ao.json").toURI()))) {
+            ao.run();
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
 }
