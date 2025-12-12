dccc422f02b5e9d9c16d7fab5531f67b2c16e664
Hahyun Baek <hahyun8587@gmail.com>
2025-12-12 19:33:22 +0900

    fix: fix message content

--- ao-framework/src/main/java/edu/handong/csee/isel/ao/examples/AgentIUA.java
+++ ao-framework/src/main/java/edu/handong/csee/isel/ao/examples/AgentIUA.java
@@ -5,6 +5,8 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.file.Path;
 
+import javax.xml.crypto.Data;
+
 import edu.handong.csee.isel.ao.Agent;
 import edu.handong.csee.isel.proto.*;
 
@@ -26,7 +28,9 @@ public class AgentIUA extends Agent {
 
     @Override
     protected RawAction calcRawAction(Data data) {
-        return RawAction.getDefaultInstance();
+        return RawAction.newBuilder()
+                        .setRawActionIUA(RawActionIUA.getDefaultInstance())
+                        .build();
     }
 
     public static void main(String[] args) {
