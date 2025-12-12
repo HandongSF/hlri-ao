654d119039d440b9caa34131663212462269f1d6
Hahyun Baek <hahyun8587@gmail.com>
2025-12-12 13:46:43 +0900

    chore: change module hierarchy

---
+++ ao-framework/src/main/java/edu/handong/csee/isel/ao/examples/AgentISA.java
@@ -0,0 +1,42 @@
+package edu.handong.csee.isel.ao.examples;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.file.Path;
+
+import edu.handong.csee.isel.ao.Agent;
+import edu.handong.csee.isel.proto.*;
+
+
+public class AgentISA extends Agent {
+
+    public AgentISA(Path config) throws IOException {
+        super(config);
+    }
+
+    @Override
+    protected void connect() throws UnknownHostException {
+        client.connect(
+                AgentInfo.newBuilder()
+                         .setType(AgentInfo.AgentType.AT_ISA)
+                         .setHost(InetAddress.getLocalHost().getHostAddress())
+                         .setPort(server.getPort())
+                         .build());
+    }
+
+    @Override
+    protected RawAction calcRawAction(Data data) {
+        System.out.println("test");
+        return RawAction.getDefaultInstance();
+    }
+
+    public static void main(String[] args) {
+        try (Agent agent = new AgentISA(
+                Path.of(AgentISA.class.getResource("/agent.json").toURI()))) {
+            agent.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
