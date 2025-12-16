1ef4f12cb2982e597bc0ac3bbc5a92f2b0dcfe31
Hahyun Baek <hahyun8587@gmail.com>
2025-12-16 15:32:57 +0900

    fix: use json tree

--- ao-core/src/main/java/edu/handong/csee/isel/ao/utils/ConfigExtractor.java
+++ ao-core/src/main/java/edu/handong/csee/isel/ao/utils/ConfigExtractor.java
@@ -3,48 +3,14 @@ package edu.handong.csee.isel.ao.utils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 
-import io.grpc.internal.JsonParser;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 
 public class ConfigExtractor {
-    private Object json;
+    protected JsonElement json;
 
     public ConfigExtractor(Path config) throws IOException {
-        json = JsonParser.parse(Files.readString(config));
-    }
-
-    public Integer getServerPort() {
-        return getPort("server");
-    }
-
-    public String getClientHost() {
-        return getHost();
-    }
-
-    public Integer getClientPort() {
-        return getPort("client");
-    }
-
-    private Integer getPort(String key) {
-        try {
-            return Integer.parseInt(getStringValue(key, "port"));
-        } catch (NumberFormatException e) {
-            return null;
-        }
-    }
-
-    private String getHost() {
-        return getStringValue("client", "host");
-    }
-
-    private String getStringValue(String outerKey, String innerKey) {
-        if (!(json instanceof Map jsonOuter
-                && jsonOuter.get(outerKey) instanceof Map jsonInner
-                && jsonInner.get(innerKey) instanceof String val)) {
-            return null;
-        }
-
-        return val; 
+        json = JsonParser.parseString(Files.readString(config));
     }
 }
