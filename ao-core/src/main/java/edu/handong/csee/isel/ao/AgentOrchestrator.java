becc3ba9623ccafa0eac7bf4ce025eb6393ecd53
Hahyun Baek <hahyun8587@gmail.com>
2025-12-16 15:34:32 +0900

    feat: support evaluation

--- ao-core/src/main/java/edu/handong/csee/isel/ao/AgentOrchestrator.java
+++ ao-core/src/main/java/edu/handong/csee/isel/ao/AgentOrchestrator.java
@@ -13,11 +13,12 @@ import com.google.protobuf.ByteString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.handong.csee.isel.ao.eval.Evaluator;
 import edu.handong.csee.isel.ao.network.ROSSimulator;
 import edu.handong.csee.isel.ao.network.client.DataStoringClient;
 import edu.handong.csee.isel.ao.network.server.ActionReceivingServer;
 import edu.handong.csee.isel.ao.policy.Router;
-import edu.handong.csee.isel.ao.utils.ConfigExtractor;
+import edu.handong.csee.isel.ao.utils.NetworkConfigExtractor;
 import edu.handong.csee.isel.proto.*;
 
 public class AgentOrchestrator implements AutoCloseable {
@@ -25,20 +26,22 @@ public class AgentOrchestrator implements AutoCloseable {
     private DataStoringClient client;
     private ROSSimulator simulator;
     private Router router;
+    private Evaluator evaluator;
     private Collection<Thread> threads;
     private Logger logger;
 
     public AgentOrchestrator(Path config) throws IOException {
-        Integer port = new ConfigExtractor(config).getServerPort();
+        Integer port = new NetworkConfigExtractor(config).getServerPort();
         
         if (port == null) {
             throw new IOException("Format of the config file is not valid");
         }
         
         server = new ActionReceivingServer(this, port);
-        client = new DataStoringClient();
+        client = new DataStoringClient(this);
         simulator = new ROSSimulator(this);
         router = new Router();
+        evaluator = new Evaluator();
         threads = new ArrayList<>();
         logger = LoggerFactory.getLogger(getClass());
     }
@@ -58,18 +61,24 @@ public class AgentOrchestrator implements AutoCloseable {
         
         runtime.addShutdownHook(shutdownHook);
         
-        simulator.start();
         server.start();
+
+        while (!client.ready());
+
+        simulator.start();
+
         server.awaitTermination(10, TimeUnit.MINUTES);
         
         logger.info("Time limit exceeded");
         runtime.removeShutdownHook(shutdownHook);
     }
 
-    public void update(AgentInfo.AgentType type, String host, int port) {
+    public void update(AgentInfo info) {
         Thread thread;
+        AgentInfo.AgentType type = info.getType();
         
-        client.addStub(type, host, port);
+        evaluator.addRecord(type);
+        client.addStub(type, info.getHost(), info.getPort());
         
         thread = new Thread(
                 () -> { 
@@ -83,8 +92,10 @@ public class AgentOrchestrator implements AutoCloseable {
     }
 
     public void update(
-            byte[] image, byte[] depth, float accel, float angular, 
-            float mag_str_x, float mag_str_y, String target, String text,
+            byte[] image, byte[] depth, int frameNum, 
+            float accel, float angular, 
+            float mag_str_x, float mag_str_y, 
+            String target, String text,
             byte[] header, byte[] format, byte[] data) {
         for (AgentInfo.AgentType type
                 : router.route(
@@ -99,8 +110,10 @@ public class AgentOrchestrator implements AutoCloseable {
                     client.addData(
                             type, 
                             convertToClientFormat(
-                                    image, depth, accel, angular, 
-                                    mag_str_x, mag_str_y, target, text));
+                                    image, depth, frameNum, 
+                                    accel, angular, 
+                                    mag_str_x, mag_str_y, 
+                                    target, text));
                     
                     break;
                 
@@ -108,7 +121,8 @@ public class AgentOrchestrator implements AutoCloseable {
                     client.addData(
                             type, 
                             convertToClientFormat(
-                                    image, depth, header, format, data));
+                                    image, depth, frameNum, 
+                                    header, format, data));
 
                     break;
 
@@ -120,50 +134,76 @@ public class AgentOrchestrator implements AutoCloseable {
     }
 
     private Data convertToClientFormat(
-            byte[] image, byte[] depth, float accel, float angular, 
-            float mag_str_x, float mag_str_y, String target, String text) {
+            byte[] image, byte[] depth, int frameNum, 
+            float accel, float angular, 
+            float mag_str_x, float mag_str_y, 
+            String target, String text) {
         Data.Builder dataBuilder = Data.newBuilder();
-        DataISA.Builder dataISABuilder = dataBuilder.getDataISABuilder();
-
-        dataISABuilder.getRgbdBuilder()
+        DataISA.Builder dataIsaBuilder = dataBuilder.getDataIsaBuilder();
+        
+        dataBuilder.setFrameNum(frameNum);
+        dataIsaBuilder.getRgbdBuilder()
                       .setImage(ByteString.copyFrom(image))
                       .setDepth(ByteString.copyFrom(depth));
-        dataISABuilder.getImuBuilder()
+        dataIsaBuilder.getImuBuilder()
                       .setAccel(accel)
                       .setAngular(angular)
                       .setMagStrX(mag_str_x)
                       .setMagStrY(mag_str_y);
-        dataISABuilder.getCmdBuilder()
+        dataIsaBuilder.getCmdBuilder()
                       .setTarget(target)
-                      .setTarget(text);
+                      .setText(text);
         
         return dataBuilder.build();
     }
 
     private Data convertToClientFormat(
-            byte[] image, byte[] depth, 
+            byte[] image, byte[] depth, int frameNum,
             byte[] header, byte[] format, byte[] data) {
         Data.Builder dataBuilder = Data.newBuilder();
-        DataIUA.Builder dataIUABuilder = DataIUA.newBuilder();
-
-        dataIUABuilder.getRgbdBuilder()
+        DataIUA.Builder dataIuaBuilder = dataBuilder.getDataIuaBuilder();
+        
+        dataBuilder.setFrameNum(frameNum);
+        dataIuaBuilder.getRgbdBuilder()
                       .setImage(ByteString.copyFrom(image))
                       .setDepth(ByteString.copyFrom(depth));
-        dataIUABuilder.getVoiceBuilder()
+        dataIuaBuilder.getVoiceBuilder()
                       .setHeader(ByteString.copyFrom(header))
                       .setFormat(ByteString.copyFrom(format))
-                      .setFormat(ByteString.copyFrom(data));
+                      .setData(ByteString.copyFrom(data));
         
         return dataBuilder.build();
     }
 
-    public void update(
-            float linearX, float linearY, float linearZ, 
-            float angularX, float angularY, float angularZ) {
-        simulator.sendAction(
-                convertToSimulatorFormat(
-                        linearX, linearY, linearZ, 
-                        angularX, angularY, angularZ));
+    public void update(Data data) {
+        evaluator.record(data);
+    }
+
+    public void update(RawAction rawAction) {
+        evaluator.evaluate(rawAction);
+        logger.info(
+                "Receiving raw action from an agent ART: {}ms acc: {}%", 
+                evaluator.getAvgReactionTime(), evaluator.getAccuracy());
+        switch (rawAction.getAgentRawActionCase()) {
+            case RAW_ACTION_ISA:
+                RawActionISA actionISA = rawAction.getRawActionIsa();
+                Linear linear = actionISA.getLinear();
+                Angular angular = actionISA.getAngular();
+
+                simulator.sendAction(
+                        convertToSimulatorFormat(
+                                linear.getX(), linear.getY(), 
+                                linear.getZ(),angular.getX(), 
+                                angular.getY(), angular.getZ()));
+                
+                break;
+
+            case RAW_ACTION_IUA:
+
+            case RAW_ACTION_IOA:
+            
+            default:
+        }
     }
 
     private String convertToSimulatorFormat(
@@ -174,10 +214,6 @@ public class AgentOrchestrator implements AutoCloseable {
                         + "angular\n\tx: %f\n\ty: %f\n\tz: %f\n",
                 linearX, linearY, linearZ, 
                 angularX, angularY, angularZ);
-    }
-
-    public void update() {
-        
     }
 
     public void close() throws InterruptedException {
@@ -199,8 +235,8 @@ public class AgentOrchestrator implements AutoCloseable {
 
     public static void main(String[] args) {
         try (AgentOrchestrator ao = new AgentOrchestrator(
-                    Path.of(AgentOrchestrator.class.getResource("/ao.json")
-                                                   .toURI()))) {
+                Path.of(AgentOrchestrator.class.getResource("/ao-network.json")
+                                               .toURI()))) {
             ao.run();
         } catch (Exception e) {
             e.printStackTrace(); 
