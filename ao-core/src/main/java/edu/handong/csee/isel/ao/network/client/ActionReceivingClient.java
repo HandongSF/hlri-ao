91b54c92b556857bbef19bc043b226dada811cfe
Hahyun Baek <hahyun8587@gmail.com>
2025-12-17 12:28:26 +0900

    chore: change logger name

--- ao-core/src/main/java/edu/handong/csee/isel/ao/network/client/ActionReceivingClient.java
+++ ao-core/src/main/java/edu/handong/csee/isel/ao/network/client/ActionReceivingClient.java
@@ -11,24 +11,25 @@ import org.slf4j.LoggerFactory;
 import edu.handong.csee.isel.proto.*;
 
 public class ActionReceivingClient {
+    private final Logger LOGGER;
     private ActionReceivingGrpc.ActionReceivingBlockingStub stub;
-    private Logger logger;
-
-    public ActionReceivingClient(String host, int port) {
+    
+    public ActionReceivingClient(String host, int port, String name) {
+        LOGGER = LoggerFactory.getLogger(name + "-client");
+        
         stub = ActionReceivingGrpc.newBlockingStub(
                 ManagedChannelBuilder.forAddress(host, port)
                                      .usePlaintext()
                                      .build());
-        logger = LoggerFactory.getLogger(getClass());
     }
 
     public void sendRawAction(RawAction action) {
-        logger.info("Sending raw action to AO server");
+        LOGGER.info("Sending raw action to AO server");
         stub.sendRawAction(action);
     }
 
     public void connect(AgentInfo info) {
-        logger.info("Connecting to AO server");
+        LOGGER.info("Connecting to AO server");
         stub.connect(info);
     }
 
