a62c69630f8aceae48a5a60751273e13eaea9ea0
Hahyun Baek <hahyun8587@gmail.com>
2025-12-20 20:40:23 +0900

    feat: add scheduler interface

--- ao-core/src/main/java/edu/handong/csee/isel/ao/policy/Scheduler.java
+++ ao-core/src/main/java/edu/handong/csee/isel/ao/policy/Scheduler.java
@@ -1,29 +1,6 @@
 package edu.handong.csee.isel.ao.policy;
 
-import java.io.IOException;
-import java.nio.file.Path;
+public abstract class Scheduler {
 
-import edu.handong.csee.isel.ao.utils.RoutingConfigExtractor;
-
-public class Scheduler {
-    private int[] numFrames;
-    private int idx;
-
-    public Scheduler(Path config) throws IOException {
-        numFrames = new RoutingConfigExtractor(config).getFrames();
-
-        if (numFrames == null) {
-            System.out.println("TEST");
-        }
-
-        idx = 0;
-    }
-
-    public int nextNumFrame() {
-        int numFrame = numFrames[idx];
-        
-        idx = (idx + 1) % numFrames.length;
-        
-        return numFrame;
-    }
+    public abstract int nextNumFrame();
 }
